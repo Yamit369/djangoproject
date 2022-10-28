e0bb8a40a6bf2c8e5e7e172b457476054a7ec4f7
Yamit <dreiko200590@hotmail.com>
2022-10-28 20:39:41 +0300

    changes on security -but still onproduction

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cunix^ltywqi-n3vy6=ry8x)pes+j3-6en=m@l9(e32&%jc^i0'
+SECRET_KEY = '############'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.80']
 
 
 # Application definition
